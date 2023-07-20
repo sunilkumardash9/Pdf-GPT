19e00bf294708facc3250773dc6bbad582cd0d1c
Sunil Kumar Dash <47926185+sunilkumardash9@users.noreply.github.com>
2023-07-20 21:59:33 +0530

    Update app.py
    corrected assigning to self.chain
    few type checks
    indentation

--- app.py
+++ app.py
@@ -15,39 +15,34 @@ import chromadb
 import re
 import uuid 
 
-enable_box = gr.Textbox.update(value=None,placeholder= 'Upload your OpenAI API key',interactive=True)
-disable_box = gr.Textbox.update(value = 'OpenAI API key is Set',interactive=False)
+enable_box = gr.Textbox.update(value = None, placeholder = 'Upload your OpenAI API key',interactive = True)
+disable_box = gr.Textbox.update(value = 'OpenAI API key is Set', interactive = False)
 
-def set_apikey(api_key):
-
-        app.OPENAI_API_KEY = api_key
-        
+def set_apikey(api_key: str):
+        app.OPENAI_API_KEY = api_key        
         return disable_box
     
 def enable_api_box():
         return enable_box
 
-
-def add_text(history, text):
+def add_text(history, text: str):
     if not text:
          raise gr.Error('enter text')
     history = history + [(text,'')] 
     return history
 
 class my_app:
-    def __init__(self, OPENAI_API_KEY= None ) -> None:
-        self.OPENAI_API_KEY = OPENAI_API_KEY
+    def __init__(self, OPENAI_API_KEY: str = None ) -> None:
+        self.OPENAI_API_KEY: str = OPENAI_API_KEY
         self.chain = None
-        self.chat_history = []
-        self.N = 0
-        self.count = 0
+        self.chat_history: list = []
+        self.N: int = 0
+        self.count: int = 0
 
-    def __call__(self, file) -> Any:
+    def __call__(self, file: str) -> Any:
         if self.count==0:
-            print('This is here')
-            self.build_chain(file)
+            self.chain = self.build_chain(file)
             self.count+=1
-
         return self.chain
     
     def chroma_client(self):
@@ -55,11 +50,9 @@ class my_app:
         client = chromadb.Client()
         #create a collecyion
         collection = client.get_or_create_collection(name="my-collection")
-
         return client
     
-    def process_file(self,file):
-
+    def process_file(self,file: str):
         loader = PyPDFLoader(file.name)
         documents = loader.load()  
         pattern = r"/([^/]+)$"
@@ -67,36 +60,30 @@ class my_app:
         file_name = match.group(1)
         return documents, file_name
     
-    def build_chain(self, file):
+    def build_chain(self, file: str):
         documents, file_name = self.process_file(file)
         #Load embeddings model
         embeddings = OpenAIEmbeddings(openai_api_key=self.OPENAI_API_KEY) 
         pdfsearch = Chroma.from_documents(documents, embeddings, collection_name= file_name,)
-
-        chain = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.0, openai_api_key=self.OPENAI_API_KEY), 
-                                                  retriever=pdfsearch.as_retriever(search_kwargs={"k": 1}),
-                                                  return_source_documents=True,)
+        chain = ConversationalRetrievalChain.from_llm(
+                ChatOpenAI(temperature=0.0, openai_api_key=self.OPENAI_API_KEY), 
+                retriever=pdfsearch.as_retriever(search_kwargs={"k": 1}),
+                return_source_documents=True,)
         return chain
     
 
-def get_response(history, query, file):
-        
-        
+def get_response(history, query, file): 
         if not file:
-            raise gr.Error(message='Upload a PDF')
-           
+            raise gr.Error(message='Upload a PDF')    
         chain = app(file)
-
         result = chain({"question": query, 'chat_history':app.chat_history},return_only_outputs=True)
         app.chat_history += [(query, result["answer"])]
         app.N = list(result['source_documents'][0])[1][1]['page']
-
         for char in result['answer']:
            history[-1][-1] += char
            yield history,''
 
 def render_file(file):
- 
         doc = fitz.open(file.name)
         page = doc[app.N]
         #Render the page as a PNG image with a resolution of 300 DPI
@@ -114,7 +101,6 @@ def render_first(file):
 
 app = my_app()
 with gr.Blocks() as demo:
-    state = gr.State(uuid.uuid4().hex)
     with gr.Column():
         with gr.Row():
             with gr.Column(scale=0.8):
@@ -135,13 +121,30 @@ with gr.Blocks() as demo:
         with gr.Column(scale=0.20):
             btn = gr.UploadButton("📁 upload a PDF", file_types=[".pdf"]).style()
         
-  
-    api_key.submit(fn=set_apikey, inputs=[api_key], outputs=[api_key,])
-    change_api_key.click(fn= enable_api_box,outputs=[api_key])
-    btn.upload(fn=render_first, inputs=[btn], outputs=[show_img,chatbot],)
+    api_key.submit(
+            fn=set_apikey, 
+            inputs=[api_key], 
+            outputs=[api_key,])
+    change_api_key.click(
+            fn= enable_api_box,
+            outputs=[api_key])
+    btn.upload(
+            fn=render_first, 
+            inputs=[btn], 
+            outputs=[show_img,chatbot],)
     
-    submit_btn.click(fn=add_text, inputs=[chatbot,txt], outputs=[chatbot, ], queue=False).success(fn=get_response,inputs = [chatbot, txt, btn],
-                                    outputs = [chatbot,txt]).success(fn=render_file,inputs = [btn], outputs=[show_img])
+    submit_btn.click(
+            fn=add_text, 
+            inputs=[chatbot,txt], 
+            outputs=[chatbot, ], 
+            queue=False).success(
+            fn=get_response,
+            inputs = [chatbot, txt, btn],
+            outputs = [chatbot,txt]).success(
+            fn=render_file,
+            inputs = [btn], 
+            outputs=[show_img]
+    )
 
     
 demo.queue()
